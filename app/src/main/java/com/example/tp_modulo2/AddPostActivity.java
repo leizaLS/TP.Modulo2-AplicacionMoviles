d018f8bdd26f247d5195a1a6b3011052a36a90f7
Leandro Solano <leandrosolano20@gmail.com>
2022-11-23 11:40:50 -0300

    publicacion sin imagenes hecha

--- app/src/main/java/com/example/tp_modulo2/AddPostActivity.java
+++ app/src/main/java/com/example/tp_modulo2/AddPostActivity.java
@@ -12,7 +12,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -21,6 +24,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
 
 public class AddPostActivity extends AppCompatActivity {
     //Utils
@@ -89,11 +97,35 @@ public class AddPostActivity extends AppCompatActivity {
                 String description = descriptionEt.getText().toString().trim();
 
                 //Subir publicacion, falta validaciones y otros
-//                pd.setMessage("Publicando...");
-//                pd.show();
-                //StorageReference ref = FirebaseStorage.getInstance().getReference();
+                pd.setMessage("Publicando...");
+                pd.show();
 
+                String timeStramp = String.valueOf(System.currentTimeMillis());
+                //String filePathAndName = "Post/" + "post_" + timeStramp;
 
+                //StorageReference ref = FirebaseStorage.getInstance().getReference().child(filePathAndName);
+
+                //Guardado de post en Firebase
+                HashMap<Object, String> hashMap = new HashMap<>();
+                hashMap.put("uid", uid);
+                hashMap.put("uName", name);
+                hashMap.put("uEmail", email);
+                hashMap.put("uDp", dp);
+                hashMap.put("pId", timeStramp);
+                hashMap.put("pTitle", title);
+                hashMap.put("pDescr", description);
+                hashMap.put("pImage", "noImage");
+                hashMap.put("pTime", timeStramp);
+
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Post");
+                ref.child(timeStramp).setValue(hashMap)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                pd.dismiss();
+                                Toast.makeText(AddPostActivity.this, "Post publicado!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         });
     }
