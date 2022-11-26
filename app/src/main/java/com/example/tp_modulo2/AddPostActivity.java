2e135ea743f8e68fe12bf7846f4819ea606253c6
Leandro Solano <leandrosolano20@gmail.com>
2022-11-25 21:46:51 -0300

    pulicaciones con fotos ok

--- app/src/main/java/com/example/tp_modulo2/AddPostActivity.java
+++ app/src/main/java/com/example/tp_modulo2/AddPostActivity.java
@@ -1,12 +1,26 @@
 package com.example.tp_modulo2;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +28,7 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -28,9 +43,21 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
+import java.security.PrivilegedAction;
 import java.util.HashMap;
 
 public class AddPostActivity extends AppCompatActivity {
+    //Permisos camara y galeria
+    private static final int CAMERA_REQUEST_CODE=100;
+    private static final int STORAGE_REQUEST_CODE=200;
+
+    private static final int IMAGE_PICK_CAMERA_CODE=300;
+    private static final int IMAGE_PICK_GALLERY_CODE=400;
+
+    String[] cameraPermissions;
+    String[] storagePermissions;
+
     //Utils
     FirebaseAuth firebaseAuth;
     DatabaseReference userDbRef;
@@ -46,6 +73,75 @@ public class AddPostActivity extends AppCompatActivity {
 
     ProgressDialog pd;
 
+    //imagen galeria
+    private void showImagePickDialog() {
+        String[] options = {"Camera", "Gallery"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Elige una imagen");
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                if (i == 0) {
+                    //desde camara
+                    if(!checkCameraPermission()){
+                        requestCameraPermission();
+                    }
+                    else {
+                        pickFromCamera();
+                    }
+                }
+                if (i == 1) {
+                    //desde galeria
+                    if(!checkStoragePermissions()){
+                        requestStoragePermission();
+                    }
+                    else {
+                        pickFromGallery();
+                    }
+                }
+            }
+        });
+        builder.create().show();
+    }
+
+    //Camara y galeria
+    private void pickFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_GALLERY_CODE);
+    }
+    private void pickFromCamera() {
+        ContentValues cv = new ContentValues();
+        cv.put(MediaStore.Images.Media.TITLE, "Temp Pick");
+        cv.put(MediaStore.Images.Media.DESCRIPTION, "Temp Descr");
+        image_rui = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, cv);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, image_rui);
+        startActivityForResult(intent, IMAGE_PICK_CAMERA_CODE);
+    }
+
+    //Revisa los permisos de storage y camara
+    private boolean checkStoragePermissions() {
+        boolean result = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+    private boolean checkCameraPermission() {
+        boolean result = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result && result1;
+    }
+    private void requestCameraPermission(){
+        ActivityCompat.requestPermissions(this, cameraPermissions, CAMERA_REQUEST_CODE);
+    }
+    private void requestStoragePermission(){
+        ActivityCompat.requestPermissions(this, cameraPermissions, STORAGE_REQUEST_CODE);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,7 +151,11 @@ public class AddPostActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Add new post");
 
-        //pd
+        //Permisos
+        cameraPermissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        storagePermissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        //ProgressBar
         pd = new ProgressDialog(this);
 
         //UsuarioActual
@@ -66,14 +166,14 @@ public class AddPostActivity extends AppCompatActivity {
         uid = user.getUid();
 
         userDbRef = FirebaseDatabase.getInstance().getReference("Users");
-
         Query query = userDbRef.orderByChild("email").equalTo(email);
         query.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for (DataSnapshot ds: snapshot.getChildren()) {
                     name = "" + ds.child("name").getValue();
-                    email = "" + ds.child("image").getValue();
+                    email = "" + ds.child("email").getValue();
+                    dp = "" + ds.child("image").getValue();
                 }
             }
 
@@ -89,6 +189,14 @@ public class AddPostActivity extends AppCompatActivity {
         imageIv = findViewById(R.id.pImageIv);
         uploadBtn = findViewById(R.id.pUploadBtn);
 
+        //Listener del seleccionar imagen
+        imageIv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showImagePickDialog();
+            }
+        });
+
         //Listener del boton Publicar
         uploadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -96,37 +204,171 @@ public class AddPostActivity extends AppCompatActivity {
                 String title = titleEt.getText().toString().trim();
                 String description = descriptionEt.getText().toString().trim();
 
-                //Subir publicacion, falta validaciones y otros
-                pd.setMessage("Publicando...");
-                pd.show();
+                if (TextUtils.isEmpty(title) || TextUtils.isEmpty(description)) {
+                    Toast.makeText(AddPostActivity.this, "Tiene campos sin completar", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
-                String timeStramp = String.valueOf(System.currentTimeMillis());
-                //String filePathAndName = "Post/" + "post_" + timeStramp;
-
-                //StorageReference ref = FirebaseStorage.getInstance().getReference().child(filePathAndName);
-
-                //Guardado de post en Firebase
-                HashMap<Object, String> hashMap = new HashMap<>();
-                hashMap.put("uid", uid);
-                hashMap.put("uName", name);
-                hashMap.put("uEmail", email);
-                hashMap.put("uDp", dp);
-                hashMap.put("pId", timeStramp);
-                hashMap.put("pTitle", title);
-                hashMap.put("pDescr", description);
-                hashMap.put("pImage", "noImage");
-                hashMap.put("pTime", timeStramp);
-
-                DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Post");
-                ref.child(timeStramp).setValue(hashMap)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                                pd.dismiss();
-                                Toast.makeText(AddPostActivity.this, "Post publicado!", Toast.LENGTH_SHORT).show();
-                            }
-                        });
+                if (image_rui == null) {
+                    uploadData(title, description, "noImage");
+                }
+                else {
+                    uploadData(title, description, String.valueOf(image_rui));
+                }
             }
         });
+    }
+
+    private void uploadData(String title, String description, String uri) {
+        pd.setMessage("Publicando...");
+        pd.show();
+
+        String timeStamp = String.valueOf(System.currentTimeMillis());
+        String filePathAndName = "Post/" + "post_" + timeStamp;
+
+        //publicar con imagen
+        if (!uri.equals("noImage")) {
+            //Toast.makeText(AddPostActivity.this, uri, Toast.LENGTH_SHORT).show();
+
+            StorageReference ref = FirebaseStorage.getInstance().getReference().child(filePathAndName);
+            ref.putFile(Uri.parse(uri))
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                            while (!uriTask.isSuccessful());
+
+                            String downloadUri = uriTask.getResult().toString();
+
+                            if(uriTask.isSuccessful()) {
+                                HashMap<Object, String> hashMap = new HashMap<>();
+                                hashMap.put("uid", uid);
+                                hashMap.put("uName", name);
+                                hashMap.put("uEmail", email);
+                                hashMap.put("uDp", dp);
+                                hashMap.put("pId", timeStamp);
+                                hashMap.put("pTitle", title);
+                                hashMap.put("pDescr", description);
+                                hashMap.put("pImage", downloadUri);
+                                hashMap.put("pTime", timeStamp);
+
+                                DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Post");
+                                ref.child(timeStamp).setValue(hashMap)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                pd.dismiss();
+                                                Toast.makeText(AddPostActivity.this, "Post publicado!", Toast.LENGTH_SHORT).show();
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                pd.dismiss();
+                                                Toast.makeText(AddPostActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                            }
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            pd.dismiss();
+                            Toast.makeText(AddPostActivity.this, "uploadtask error", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+        //publicar sin imagen
+        else {
+            HashMap<Object, String> hashMap = new HashMap<>();
+            hashMap.put("uid", uid);
+            hashMap.put("uName", name);
+            hashMap.put("uEmail", email);
+            hashMap.put("uDp", dp);
+            hashMap.put("pId", timeStamp);
+            hashMap.put("pTitle", title);
+            hashMap.put("pDescr", description);
+            hashMap.put("pImage", "noImage");
+            hashMap.put("pTime", timeStamp);
+
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Post");
+            ref.child(timeStamp).setValue(hashMap)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            pd.dismiss();
+                            Toast.makeText(AddPostActivity.this, "Post publicado!", Toast.LENGTH_LONG).show();
+
+                            //Reset contenido
+                            titleEt.setText("");
+                            descriptionEt.setText("");
+                            imageIv.setImageURI(null);
+                            image_rui = null;
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            pd.dismiss();
+                            Toast.makeText(AddPostActivity.this, "Error al intentar publicar", Toast.LENGTH_LONG).show();
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case CAMERA_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean storageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                    if (cameraAccepted && storageAccepted) {
+                        pickFromCamera();
+                    }
+                    else {
+                        Toast.makeText(this, "Error permisos", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+
+                }
+            }
+            break;
+
+            case STORAGE_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    boolean storageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (storageAccepted) {
+                        pickFromGallery();
+                    }
+                    else {
+                        Toast.makeText(this, "Error permisos", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+
+                }
+            }
+            break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == IMAGE_PICK_GALLERY_CODE) {
+                image_rui = data.getData();
+                imageIv.setImageURI(image_rui);
+            }
+            else if (requestCode == IMAGE_PICK_CAMERA_CODE) {
+                imageIv.setImageURI(image_rui);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
